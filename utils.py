2c1fdeace72bf4f16427d0aeb72d2d520d7f5042
hjc <1067191099@qq.com>
2024-10-28 17:08:59 +0800

    [hjc] data load method modification

--- utils.py
+++ utils.py
@@ -1,14 +1,20 @@
 import os
+import json
 import torch
 from datasets import load_dataset
 
 
 class AudioSetSLPreprocessor():
-    def __init__(self, processor, audio_dir, device):
+    def __init__(self, processor, json_file):
         self.processor = processor
-        self.audio_dir = audio_dir
-        self.device = device
-
+        
+        with open(json_file, "r", encoding="utf-8") as f:
+            json_data = json.load(f)
+        segment_id2data = dict()
+        for data_i in json_data:
+            segment_id2data[data_i["segment_id"]] = {"class_labels": data_i["class_labels"], "boxes": data_i["boxes"]}
+        self.json_data = segment_id2data
+        
     def labels2ids(self, class_labels, boxes):
         class_labels_ids = list()
         boxes_labels_ids = list()
@@ -36,18 +42,17 @@ class AudioSetSLPreprocessor():
 
     def __call__(self, examples):
         text_list, audio_list, labels_list = [], [], []
-        for i in range(len(examples["segment_id"])):
-            sentence, class_labels_ids, boxes_labels_ids = self.labels2ids(examples["class_labels"][i], examples["boxes"][i])
-            audio_file_path = os.path.join(self.audio_dir, examples["segment_id"][i]+".wav")
-
-            audio_list.append(audio_file_path)
+        for i in range(len(examples["audio"])):
+            segment_id = examples["audio"][i]["path"].rsplit("/", 1)[-1].split(".", 1)[0]
+            sentence, class_labels_ids, boxes_labels_ids = self.labels2ids(self.json_data[segment_id]["class_labels"], self.json_data[segment_id]["boxes"])
+            audio_list.append(examples["audio"][i]["array"])
             text_list.append(sentence)
             
             labels_list.append({
-                "class_labels": torch.tensor(class_labels_ids, device=self.device, dtype=torch.long),
-                "boxes": torch.tensor(boxes_labels_ids, device=self.device)
+                "class_labels": torch.tensor(class_labels_ids, dtype=torch.long),
+                "boxes": torch.tensor(boxes_labels_ids)
             })
-        inputs = self.processor(audios=audio_list, text=text_list, device=self.device)
+        inputs = self.processor(audios=audio_list, text=text_list)
         inputs["labels"] = labels_list
 
         return inputs
